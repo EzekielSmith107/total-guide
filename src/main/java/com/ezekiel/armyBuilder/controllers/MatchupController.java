061e66278d12c3723ce3bd3b85a12bd66faa0ba8
EzekielSmith107 <ezesmith107@gmail.com>
2022-08-31 12:07:53 -0400

    endpoints operational

--- src/main/java/com/ezekiel/armyBuilder/controllers/MatchupController.java
+++ src/main/java/com/ezekiel/armyBuilder/controllers/MatchupController.java
@@ -3,10 +3,7 @@ package com.ezekiel.armyBuilder.controllers;
 import com.ezekiel.armyBuilder.dtos.MatchupDto;
 import com.ezekiel.armyBuilder.services.MatchupService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +13,7 @@ public class MatchupController {
     @Autowired
     private MatchupService matchupService;
 
-    @GetMapping("/add")
+    @PostMapping("/add")
     public List<String> addMatchup(@RequestBody MatchupDto matchupDto, Long buildId) {
         return matchupService.addMatchup(matchupDto, buildId);
     }
