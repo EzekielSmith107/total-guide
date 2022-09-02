0ac4049a471840026bbdb19981f15f17d9d53e01
EzekielSmith107 <ezesmith107@gmail.com>
2022-09-02 13:28:22 -0400

    working on cheatsheet structure

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
 import Guidelines from "./components/guidelines/Guidelines";
+import Cheatsheet from "./components/cheatsheet/Cheatsheet";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <Guidelines />
+      <Cheatsheet />
     </div>
   );
 }
---
+++ src/main/frontend/src/components/cheatsheet/Cheatsheet.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import classes from './Cheatsheet.module.css'
+
+const Cheatsheet = () => {
+  return (
+    <div className={classes.cheatsheetContainer}>
+        <h1>Cheatsheet Overview</h1>
+        <div className={classes.textContainer}>
+            <p>Here is a small paragraph explaining the cheatsheet. I hope people can use this to help them learn more about the game and remove barriers to entry. My goal is to make a fun and creative design to appeal to people of all backgrounds. The cheatsheet will be downloadable for all.</p>
+        </div>
+        <button>Download Cheatsheet</button>
+        <div className={classes.showcaseContainer}>
+            <h1>Showcase Battle</h1>
+            <iframe width="942" height="530" src="https://www.youtube.com/embed/weA8KxPOyy4" title="THOSE DASTARDLY DEPTH GUARD - Vampire Coast vs. Norsca - Total War Warhammer 3" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </div>
+  )
+}
+
+export default Cheatsheet
