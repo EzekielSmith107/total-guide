5d3a45dc2576b8c7c479beee00282b9760be1d39
EzekielSmith107 <ezesmith107@gmail.com>
2022-09-05 10:42:28 -0400

    added faction options

--- src/main/frontend/src/components/generator/Generator.js
+++ src/main/frontend/src/components/generator/Generator.js
@@ -13,6 +13,25 @@ const Generator = () => {
                     <option value='beastmen'>Beastmen</option>
                     <option value='bretonnia'>Bretonnia</option>
                     <option value='dark elves'>Dark Elves</option>
+                    <option value='dwarfs'>Dwarfs</option>
+                    <option value='empire'>Empire</option>
+                    <option value='grand cathay'>Grand Cathay</option>
+                    <option value='greenskins'>Greenskins</option>
+                    <option value='high elves'>High Elves</option>
+                    <option value='khorne'>Khorne</option>
+                    <option value='kislev'>Kislev</option>
+                    <option value='lizardmen'>Lizardmen</option>
+                    <option value='norsca'>Norsca</option>
+                    <option value='nurgle'>Nurgle</option>
+                    <option value='ogre kingdoms'>Ogre Kingdoms</option>
+                    <option value='skaven'>Skaven</option>
+                    <option value='slaanesh'>Slaanesh</option>
+                    <option value='tomb kings'>Tomb Kings</option>
+                    <option value='tzeentch'>Tzeentch</option>
+                    <option value='vampire coast'>Vampire Coast</option>
+                    <option value='vampire counts'>Vampire Counts</option>
+                    <option value='warriors of chaos'>Warriors of Chaos</option>
+                    <option value='wood elves'>Wood Elves</option>
                 </select>
             </div>
             <div className={classes.select}>
@@ -21,6 +40,25 @@ const Generator = () => {
                     <option value='beastmen'>Beastmen</option>
                     <option value='bretonnia'>Bretonnia</option>
                     <option value='dark elves'>Dark Elves</option>
+                    <option value='dwarfs'>Dwarfs</option>
+                    <option value='empire'>Empire</option>
+                    <option value='grand cathay'>Grand Cathay</option>
+                    <option value='greenskins'>Greenskins</option>
+                    <option value='high elves'>High Elves</option>
+                    <option value='khorne'>Khorne</option>
+                    <option value='kislev'>Kislev</option>
+                    <option value='lizardmen'>Lizardmen</option>
+                    <option value='norsca'>Norsca</option>
+                    <option value='nurgle'>Nurgle</option>
+                    <option value='ogre kingdoms'>Ogre Kingdoms</option>
+                    <option value='skaven'>Skaven</option>
+                    <option value='slaanesh'>Slaanesh</option>
+                    <option value='tomb kings'>Tomb Kings</option>
+                    <option value='tzeentch'>Tzeentch</option>
+                    <option value='vampire coast'>Vampire Coast</option>
+                    <option value='vampire counts'>Vampire Counts</option>
+                    <option value='warriors of chaos'>Warriors of Chaos</option>
+                    <option value='wood elves'>Wood Elves</option>
                 </select>
             </div>
         </div>
