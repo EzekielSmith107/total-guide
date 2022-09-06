913c0ea16f9fd104a52fb39b424f3b1169c7f92e
EzekielSmith107 <ezesmith107@gmail.com>
2022-09-06 14:59:02 -0400

    working on scroll css

--- src/main/frontend/src/components/navbar/Navbar.js
+++ src/main/frontend/src/components/navbar/Navbar.js
@@ -8,18 +8,18 @@ const Navbar = () => {
     <div className={classes.nav}>
         <div>
             <ul className={classes.navbarContainer}>
-                <Link className={classes.navLogo} to="home" spy={true} smooth={true} offset={-80} duration={500}>The Gentle Rat</Link>
+                <Link className={classes.navLogo} to="home" spy={true} smooth={true} offset={-90} duration={500}>The Gentle Rat</Link>
                 <li className={classes.navItem}>
-                    <Link className={classes.navLinks} to="guidelines" spy={true} smooth={true} offset={-80} duration={500}>Guidelines</Link>
+                    <Link className={classes.navLinks} to="guidelines" spy={true} smooth={true} offset={-90} duration={500}>Guidelines</Link>
                 </li>
                 <li className={classes.navItem}>
-                    <Link className={classes.navLinks} to="cheatsheet" spy={true} smooth={true} offset={-80} duration={500}>Cheatsheet</Link>
+                    <Link className={classes.navLinks} to="cheatsheet" spy={true} smooth={true} offset={-90} duration={500}>Cheatsheet</Link>
                 </li>
                 <li className={classes.navItem}>
-                    <Link className={classes.navLinks} to="generator" spy={true} smooth={true} offset={-80} duration={500}>Generator</Link>
+                    <Link className={classes.navLinks} to="generator" spy={true} smooth={true} offset={-90} duration={500}>Generator</Link>
                 </li>
                 <li className={classes.navItem}>
-                    <Link className={classes.navLinks} to="resources" spy={true} smooth={true} offset={-80} duration={500}>Resources</Link>
+                    <Link className={classes.navLinks} to="resources" spy={true} smooth={true} offset={-90} duration={500}>Resources</Link>
                 </li>
             </ul>
         </div>
