6a0dc75ced5059e3cd47e5a3140527b4f142fd34
EzekielSmith107 <ezesmith107@gmail.com>
2022-09-12 14:09:32 -0400

    working on cheat sheet

--- src/main/frontend/src/components/cheatsheet/Cheatsheet.js
+++ src/main/frontend/src/components/cheatsheet/Cheatsheet.js
@@ -1,6 +1,6 @@
 import classes from './Cheatsheet.module.css'
 import Card from '../card/Card'
-import testImage from '../../images/testImageOne.jpg'
+import cheatSheet from '../../images/Cheat Sheet.pdf'
 
 const Cheatsheet = () => {
   return (
@@ -10,7 +10,7 @@ const Cheatsheet = () => {
             <h1>Cheat Sheet Overview</h1>
             <p>I have collected some general guidelines for new players when building an army and made an infographic to help simplify the process. Remember that these are just general guidelines and should not be taken as "law". Some of the top players in the world will take super optimized and skewed armies of entirely calvary or a massive wave of archers. When you feel more confident, I definitely recommend jumping in and building new armies that fit your playstyle and are the most fun to you. You can download the infographic below and also view a showcase match of me using an army built from the general guidelines in an actual multiplayer match.</p>
             <button className={classes.downloadBtn}>
-              <a href={testImage} download='' target='blank'>Download Cheat Sheet</a>
+              <a href={cheatSheet} download='' target='blank'>Download Cheat Sheet</a>
             </button>
         </Card>
         </div>
