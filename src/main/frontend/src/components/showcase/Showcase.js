26a0d870c9783f7eff3162ac12c5915b78480f04
EzekielSmith107 <ezesmith107@gmail.com>
2022-09-12 17:38:43 -0400

    fixed video fullscreen

--- src/main/frontend/src/components/showcase/Showcase.js
+++ src/main/frontend/src/components/showcase/Showcase.js
@@ -5,7 +5,7 @@ const Showcase = () => {
     <div className={classes.showcaseContainer} id='showcase'>
         <hr/>
         <h1>Showcase Battle</h1>
-        <iframe width="942" height="530" src="https://www.youtube.com/embed/bzXEIkheGX8" title="TWW3 Showcase Match" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="942" height="530" src="https://www.youtube.com/embed/bzXEIkheGX8" title="TWW3 Showcase Match" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
   )
 }
