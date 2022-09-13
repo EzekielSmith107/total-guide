c6d1c338720782f0887f771a8eacac0a08ecd08a
EzekielSmith107 <ezesmith107@gmail.com>
2022-09-13 15:32:34 -0400

    reset navbar for mobile refactor

--- src/main/frontend/src/components/cheatsheet/Cheatsheet.js
+++ src/main/frontend/src/components/cheatsheet/Cheatsheet.js
@@ -9,9 +9,9 @@ const Cheatsheet = () => {
           <Card>
             <h1>Cheat Sheet Overview</h1>
             <p>I have collected some general guidelines for new players when building an army and made an infographic to help simplify the process. Remember that these are just general guidelines and should not be taken as "law". Some of the top players in the world will take super optimized and skewed armies of entirely calvary or a massive wave of archers. When you feel more confident, I definitely recommend jumping in and building new armies that fit your playstyle and are the most fun to you. You can download the infographic below and also view a showcase match of me using an army built from the general guidelines in an actual multiplayer match.</p>
-            <button className={classes.downloadBtn}>
-              <a href={cheatSheet} download='' target='blank'>Download Cheat Sheet</a>
-            </button>
+            <a href={cheatSheet} download='' target='blank' >
+              <button className={classes.downloadBtn}>Download Cheat Sheet</button>
+            </a>
         </Card>
         </div>
     </div>
--- src/main/frontend/src/components/navbar/Navbar.js
+++ src/main/frontend/src/components/navbar/Navbar.js
@@ -1,33 +1,14 @@
 import { Link } from 'react-scroll'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div className={classes.nav}>
-        <div>
-            <ul className={classes.navbarContainer}>
-                <li className={classes.navItem}>
-                    <Link className={classes.navLinks} to="home" spy={true} smooth={true} offset={-90} duration={500}>The Gentle Rat</Link>
-                </li>
-                <li className={classes.navItem}>
-                    <Link className={classes.navLinks} to="guidelines" spy={true} smooth={true} offset={-90} duration={500}>Guidelines</Link>
-                </li>
-                <li className={classes.navItem}>
-                    <Link className={classes.navLinks} to="cheatsheet" spy={true} smooth={true} offset={-90} duration={500}>Cheat Sheet</Link>
-                </li>
-                <li className={classes.navItem}>
-                    <Link className={classes.navLinks} to="showcase" spy={true} smooth={true} offset={-90} duration={500}>Showcase</Link>
-                </li>
-                <li className={classes.navItem}>
-                    <Link className={classes.navLinks} to="generator" spy={true} smooth={true} offset={-90} duration={500}>Generator</Link>
-                </li>
-                <li className={classes.navItem}>
-                    <Link className={classes.navLinks} to="resources" spy={true} smooth={true} offset={-90} duration={500}>Resources</Link>
-                </li>
-            </ul>
-        </div>
-    </div>
+    <nav>
+        
+    </nav>
   )
 }
 
